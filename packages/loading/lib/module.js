2aa2cd63d35584b0240cbd65a24f59e75791ed65
debs-obrien <debs_obrien@yahoo.co.uk>
2020-08-13 19:27:57 +0200

    fix: remove console.log

--- packages/loading/lib/module.js
+++ packages/loading/lib/module.js
@@ -5,7 +5,6 @@ module.exports = function NuxtLoadingScreen () {
 
   const defu = require('defu')
   const LoadingUI = require('./loading')
-console.log('Debbie')
   const { nuxt } = this
 
   // Tip
