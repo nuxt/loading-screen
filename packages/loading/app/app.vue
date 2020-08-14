d4de7179ca61245522113ba15ea5f40e3c18d131
debs-obrien <debs_obrien@yahoo.co.uk>
2020-08-14 18:39:52 +0200

    styles: new styling

--- packages/loading/app/app.vue
+++ packages/loading/app/app.vue
@@ -17,25 +17,6 @@
         </template>
       </transition>
     </div>
-    <div class="row">
-      <div class="alert">
-        <div class="alert-wrapper">
-          <div class="alert-icon">
-            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="alert-content">
-            <p v-html="options.tip.text" />
-            <p><a :href="options.tip.link">See the docs for more info</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
     <div v-if="!bundles.length && !hasErrors && !preventReload" class="row placeholder">
       <transition appear>
         <h3>Loading...</h3>
@@ -80,6 +61,21 @@
         <h4>{{ states[bundle].status }}</h4>
       </div>
     </transition-group>
+    <transition appear>
+      <div class="row">
+        <div class="alert">
+          <div class="alert-wrapper">
+            <div class="alert-icon">
+              <svg viewBox="0 0 20 20" fill="currentColor" class="light-bulb w-6 h-6"><path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z"></path></svg>
+            </div>
+            <div class="alert-content">
+              <p v-html="options.tip.text" />
+              <p><a :href="options.tip.link">See the docs for more info</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
